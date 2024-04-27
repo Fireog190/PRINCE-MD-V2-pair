ce6354cc3c803ebcb3f20e8c889c6d3e74d925f9
Freddy Phoenix Mills <118795606+anonphoenix007@users.noreply.github.com>
2024-04-27 23:16:09 +0100

    Update Excel.js

--- Excel.js
+++ Excel.js
@@ -25,80 +25,9 @@ const PORT = process.env.PORT || 8000;
 //let server = require('./qr');
 //let  code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
-//app.use('/qr', server);
-//app.use('/code', code);
-app.use("/qrcode", async(req, res) => {
-  if (fs.existsSync('./auth_info_baileys')) {
-    fs.emptyDirSync(__dirname + '/auth_info_baileys');
-  };
-  const { default: SuhailWASocket, useMultiFileAuthState, Browsers, delay,DisconnectReason, makeInMemoryStore, } = require("@whiskeysockets/baileys");
-  const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
-  async function SUHAIL() {
-    const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys')
-    try {
-      let Smd =SuhailWASocket({ 
-        printQRInTerminal: false,
-        logger: pino({ level: "silent" }), 
-        browser: Browsers.baileys("Desktop"),
-        auth: state 
-        });
-
-
-      Smd.ev.on("connection.update", async (s) => {
-        const { connection, lastDisconnect, qr } = s;
-        if (qr) { res.end(await toBuffer(qr)); }
-
-
-        if (connection == "open"){
-          await delay(3000);
-          let user = Smd.user.id;
-          let CREDS = fs.readFileSync(__dirname + '/auth_info_baileys/creds.json')
-          const xeonses = await  Smd.sendMessage(user, { document: CREDS, mimetype: `application/json`, fileName: `creds.json` });
-          let session_id = await smd.sendMessage(user, xeonses); 
-          await Smd.sendMessage(user, session_id);
-          //let msgsss = await Smd.sendMessage(user, { text:  Scan_Id });
-          await Smd.sendMessage(user, { text: MESSAGE } , { quoted : msgsss });
-          await delay(1000);
-          try{ await fs.emptyDirSync(__dirname+'/auth_info_baileys'); }catch(e){}
-
-
-        }
-
-        Smd.ev.on('creds.update', saveCreds)
-
-        if (connection === "close") {            
-            let reason = new Boom(lastDisconnect?.error)?.output.statusCode
-            // console.log("Reason : ",DisconnectReason[reason])
-            if (reason === DisconnectReason.connectionClosed) {
-              console.log("Connection closed!")
-             // SUHAIL().catch(err => console.log(err));
-            } else if (reason === DisconnectReason.connectionLost) {
-                console.log("Connection Lost from Server!")
-            //  SUHAIL().catch(err => console.log(err));
-            } else if (reason === DisconnectReason.restartRequired) {
-                console.log("Restart Required, Restarting...")
-              SUHAIL().catch(err => console.log(err));
-            } else if (reason === DisconnectReason.timedOut) {
-                console.log("Connection TimedOut!")
-             // SUHAIL().catch(err => console.log(err));
-            }  else {
-                console.log('Connection closed with bot. Please run again.');
-                console.log(reason)
-              //process.exit(0)
-            }
-          }
-
-
-
-      });
-    } catch (err) {
-        console.log(err);
-       await fs.emptyDirSync(__dirname+'/auth_info_baileys'); 
-    }
-  }
+app.use("/scan", async(req, res, next) => {
+res.sendFile(__path + './qrcode.html')
 })
- 
-
 app.use('/pair',async (req, res, next) => {
 res.sendFile(__path + '/pair.html')
 })
